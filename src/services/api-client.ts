db2315e352881fea3a439d2caffa27fdc8aa328b
liucuxiu <xliucuxiux@gmail.com>
2023-05-29 21:04:02 +0700

    refactor fetch data with generic type

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.rawg.io/api/',
   params: {
-    key: 'e1103af34bc94fa1b5c6d04f1b39fec6aaaa'
+    key: 'e1103af34bc94fa1b5c6d04f1b39fec6'
   }
 })
