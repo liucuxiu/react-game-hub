2323ccf206c437ee66990b607954351e58f5a17a
liucuxiu <xliucuxiux@gmail.com>
2023-05-29 21:22:15 +0700

    display Genre

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const GameCardContainer = ({ children }: Props) => {
   return (
-    <Box width='300px' height='400px' borderRadius={10} overflow='hidden'>
+    <Box width='100%' height='400px' borderRadius={10} overflow='hidden'>
       {children}
     </Box>
   )
