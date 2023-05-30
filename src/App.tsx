c2e9ebcc61dbc4df8bdf77478a442dd4f0eb5a9c
liucuxiu <xliucuxiux@gmail.com>
2023-05-30 08:52:37 +0700

    build platforms selector

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import GameGrid from './components/GameGrid';
 import GenreList from './components/GenreList';
 import { useState } from 'react';
 import { Genre } from './hooks/useGenres';
+import PlatformSelector from './components/PlatformSelector';
 
 
 function App() {
@@ -18,9 +19,7 @@ function App() {
       templateColumns={{
         base: '1fr',
         lg: '200px 1fr'
-      }}
-
-    >
+      }}>
       <GridItem area='nav'>
         <NavBar/>
       </GridItem>
@@ -30,6 +29,7 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area='main'>
+        <PlatformSelector/>
         <GameGrid selectedGenre={selectedGenre}/>
       </GridItem>
 
