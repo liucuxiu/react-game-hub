a418aa0137f42b48e757ed92b9c8d6ce6830b55c
liucuxiu <xliucuxiux@gmail.com>
2023-06-01 08:49:01 +0700

    custom charka theme color

--- src/theme.ts
+++ src/theme.ts
@@ -4,6 +4,17 @@ const config: ThemeConfig = {
   initialColorMode: "dark",
 }
 
-const theme = extendTheme({ config })
+const theme = extendTheme({
+  config,
+  colors: {
+    gray: {
+      50: '#f9f9f9',
+      100: '#f2f2f2',
+      200: '#e6e6e6',
+      300: '#cccccc',
+      400: '#b3b3b3',
+    }
+  }
+})
 
 export default theme;
